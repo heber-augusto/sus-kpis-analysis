d0ad8d460b52297961842a7d220f6b7f7885ff49
Heber Scachetti <heber.augusto@gmail.com>
2022-11-26 11:36:29 -0300

    Update create-base-kpis.py

--- sia/etls/create-base-kpis.py
+++ sia/etls/create-base-kpis.py
@@ -30,7 +30,7 @@ def get_files(state, year, month, file_type, file_group):
     initial_path = input_dir
     internal_folder = f"""{state}/{year}/{month}/{file_type}/{file_group}"""
     glob_filter = f"{initial_path}/{internal_folder}/*.parquet.gzip"
-    print(f'glob_filter: {glob_filter}')
+    # print(f'glob_filter: {glob_filter}')
     return glob.glob(glob_filter)
 
 # SIA PA, AQ e AR: Leitura, filtro e transformação inicial dos arquivos
@@ -121,22 +121,21 @@ def create_cancer_dataframe(file_paths, filter_function=filter_pa_content):
         ignore_index=True)
 
 ## Função para retornar lista de arquivos (caminho completo)
-def get_file_paths(states, years, months, file_type, file_group):
+def get_file_paths(state, years, months, file_type, file_group):
     """
 
     """
     file_paths = []
-    for state in states:
-        for year in years:
-            for month in months:
-                file_paths.extend(
-                    get_files(
-                        state,
-                        year,
-                        month,
-                        file_type,
-                        file_group)
-                )
+    for year in years:
+        for month in months:
+            file_paths.extend(
+                get_files(
+                    state,
+                    year,
+                    month,
+                    file_type,
+                    file_group)
+            )
     return file_paths
 
 ## SIA PA: Leitura e união de dados para o período desejado
@@ -147,44 +146,65 @@ years  = [f'{year + 2008:02d}' for year in range(end_year - start_year + 1)]
 months = [f'{month + 1:02d}' for month in range(12)]
 file_type = 'SIA'
 
+for state in states:
+    print('processing files from SP')
+    ### Monta lista de arquivos a serem lidos
+    file_paths_by_type = {}
 
-### Monta lista de arquivos a serem lidos
-file_paths_by_type = {}
+    # Arquivos de produção ambulatorial
+    file_paths_by_type['PA'] = get_file_paths(
+        state,
+        years,
+        months,
+        file_type,
+        'PA'
+    )
 
-# Arquivos de produção ambulatorial
-file_paths_by_type['PA'] = get_file_paths(
-    states,
-    years,
-    months,
-    file_type,
-    'PA'
-)
+    # Arquivos de radioterapia
+    file_paths_by_type['AR'] = get_file_paths(
+        state,
+        years,
+        months,
+        file_type,
+        'AR'
+    )
 
-# Arquivos de radioterapia
-file_paths_by_type['AR'] = get_file_paths(
-    states,
-    years,
-    months,
-    file_type,
-    'AR'
-)
+    # Arquivos de quimioteraia
+    file_paths_by_type['AQ'] = get_file_paths(
+        state,
+        years,
+        months,
+        file_type,
+        'AQ'
+    )
 
-# Arquivos de quimioteraia
-file_paths_by_type['AQ'] = get_file_paths(
-    states,
-    years,
-    months,
-    file_type,
-    'AQ'
-)
-
-print(f"""Identificados {len(file_paths_by_type['PA'])} arquivos de produção ambulatorial""")
-print(f"""Identificados {len(file_paths_by_type['AR'])} arquivos de radioterapia""")
-print(f"""Identificados {len(file_paths_by_type['AQ'])} arquivos de quimioterapia""")
-
-
-
+    print(f"""Identificados {len(file_paths_by_type['PA'])} arquivos de produção ambulatorial para {state}""")
+    print(f"""Identificados {len(file_paths_by_type['AR'])} arquivos de radioterapia para {state}""")
+    print(f"""Identificados {len(file_paths_by_type['AQ'])} arquivos de quimioterapia para {state}""")
 
 
+    # Montagem do dataset cancer AR e AQ
+
+    # Estes dois datasets (cancer_aq e cancer AR) não realizam nenhum tipo de filtro ou transformação 
+    # nos dados originais, apenas consolidam os registros de todos os anos/meses, coletados do FTP do SUS, 
+    # em um único dataframe.
+
+    cancer_dataframe_pa = create_cancer_dataframe(file_paths_by_type['PA'], filter_function=filter_pa_content)
+    cancer_dataframe_aq = create_cancer_dataframe(file_paths_by_type['AQ'], filter_function=filter_ar_content)
+    cancer_dataframe_ar = create_cancer_dataframe(file_paths_by_type['AR'], filter_function=filter_aq_content)
+
+    destination_folder = f"""{output_dir}/{state}/consolidado/"""
+    
+    cancer_dataframe_pa.to_parquet(
+        f'{destination_folder}cancer_pa.parquet.gzip', 
+        compression='gzip')
+
+    cancer_dataframe_aq.to_parquet(
+        f'{destination_folder}cancer_aq.parquet.gzip', 
+        compression='gzip')
+
+    cancer_dataframe_ar.to_parquet(
+        f'{destination_folder}cancer_ar.parquet.gzip', 
+        compression='gzip')
 
 
