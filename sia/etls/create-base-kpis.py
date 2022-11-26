3a3756d2f6c2ebcc17f47adad347bcdbae355073
Heber Scachetti <heber.augusto@gmail.com>
2022-11-26 11:19:19 -0300

    Update create-base-kpis.py

--- sia/etls/create-base-kpis.py
+++ sia/etls/create-base-kpis.py
@@ -20,7 +20,10 @@ print(output_dir)
 temp_dir   = os.getenv('TEMP_DIR', os.path.join(home_dir,'temp-files'))
 print(temp_dir)
 
-states   = os.getenv('STATES', ['SP',])
+states   = os.getenv('STATES', 'SP'])
+if states == '':
+    states = 'SP'
+states = states.split(',')
 print(states)
 
 def get_files(state, year, month, file_type, file_group):
