729fc44135d2745621f8ff7dd9adef88f1aba9e9
Heber Scachetti <heber.augusto@gmail.com>
2023-09-27 11:17:48 -0300

    Update setup_spark_env.sh

--- sia/etls/bin/setup_spark_env.sh
+++ sia/etls/bin/setup_spark_env.sh
@@ -11,9 +11,4 @@ wget -q https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-late
 
 echo "setting enviroment variables";
 
-export PYTHONHASHSEED=1234
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export SPARK_HOME="${1}spark-3.4.1-bin-hadoop3"
-export SPARK_VERSION=3.4.1
-
 mv "${1}gcs-connector-hadoop3-latest.jar" $SPARK_HOME/jars
