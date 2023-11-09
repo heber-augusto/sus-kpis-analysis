f3a584ace439491eaf1124719711bd21efaf6319
Heber Scachetti <heber.augusto@gmail.com>
2023-11-09 08:46:30 -0300

    Create bronze_files_utilities.py

---
+++ sia/etls/lib/bronze_files_utilities.py
@@ -0,0 +1,63 @@
+from google.cloud import storage
+from pyspark.sql.functions import col
+from pyspark.sql.functions import to_timestamp
+import re
+
+def get_parquet_files_from_gs_storage(storage_client, bucket_name, prefix_pattern):
+    # Acesse o bucket desejado
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # Use expressão regular para criar um padrão para o prefixo
+    prefix_regex = re.compile(prefix_pattern)
+
+    # Liste todos os objetos no bucket
+    blobs = bucket.list_blobs()
+
+    # Filtrar apenas os arquivos Parquet que correspondem ao padrão do prefixo
+    arquivos_parquet_com_data = []
+
+    for blob in blobs:
+        if blob.name.endswith('.parquet.gzip') and prefix_regex.match(blob.name):
+            arquivo_info = {
+                'caminho': f"""gs://{bucket_name}/{blob.name}""",
+                'data_criacao': blob.time_created
+            }
+            arquivos_parquet_com_data.append(arquivo_info)
+
+    # Crie um DataFrame a partir da lista de dicionários
+    df = spark.createDataFrame(arquivos_parquet_com_data)
+
+    # Ajuste o formato da coluna 'data_criacao' para corresponder ao formato desejado
+    df = df.withColumn("file_modification_time", to_timestamp(col("data_criacao"), "yyyy-MM-dd HH:mm:ss.SSS"))
+
+    # Renomeie a coluna 'caminho' para '_filepath'
+    df = df.withColumnRenamed("caminho", "_filepath")
+
+    df = df.select('_filepath','file_modification_time')
+
+    return df
+
+def get_pending_files_from_bronze(storage_client, bucket_name, spark_session, file_group, file_type):
+    prefix_pattern = f'{project_folder_name}/[^/]+/[^/]+/[^/]+/{file_group.upper()}/{file_type.upper()}/[^/]+\.parquet\.gzip'
+    parquet_df = get_parquet_files_from_gs_storage(
+        storage_client, 
+        bucket_name, 
+        prefix_pattern)
+
+    parquet_df_with_filepath = (parquet_df
+        .selectExpr("_filepath", "file_modification_time as current_file_modification_time")
+    ).distinct()
+
+    processed_files_df = spark_session.sql(f"""
+      SELECT distinct _filepath, file_modification_time
+      FROM {file_group.lower()}_bronze.{file_type.lower()}""")
+    
+    pending_files = parquet_df_with_filepath.join(
+        processed_files_df,
+        parquet_df_with_filepath._filepath == processed_files_df._filepath,
+        how = 'left'
+    ).where("""
+    (file_modification_time is NULL) OR 
+    (current_file_modification_time != file_modification_time)
+    """)
+    return [row['_filepath'] for row in pending_files.collect()]
