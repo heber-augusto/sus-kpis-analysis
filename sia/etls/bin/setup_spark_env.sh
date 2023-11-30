8ddb6e989dd6589f35289499b48e3cfc8a6b4dcd
Heber Scachetti <heber.augusto@gmail.com>
2023-11-30 13:41:05 -0300

    Update setup_spark_env.sh

--- sia/etls/bin/setup_spark_env.sh
+++ sia/etls/bin/setup_spark_env.sh
@@ -4,11 +4,11 @@ echo "installing and downloading packages";
 
 apt-get update  > /dev/null
 apt-get install openjdk-8-jdk-headless -qq > /dev/null
-wget -q https://downloads.apache.org/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz
-tar xf spark-3.4.1-bin-hadoop3.tgz
+wget -q https://downloads.apache.org/spark/spark-3.4.2/spark-3.4.2-bin-hadoop3.tgz
+tar xf spark-3.4.2-bin-hadoop3.tgz
 
 wget -q https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar
 
 echo "setting enviroment variables";
 
-mv "${1}gcs-connector-hadoop3-latest.jar" "${1}/spark-3.4.1-bin-hadoop3/jars"
+mv "${1}gcs-connector-hadoop3-latest.jar" "${1}/spark-3.4.2-bin-hadoop3/jars"
