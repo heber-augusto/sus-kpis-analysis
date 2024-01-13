58a69ff3a294c81660d81716a2e7c9e3a2f34937
Heber Scachetti <heber.augusto@gmail.com>
2024-01-13 16:32:16 -0300

    Update mount_google_drive.sh

--- sia/etls/bin/mount_google_drive.sh
+++ sia/etls/bin/mount_google_drive.sh
@@ -9,4 +9,4 @@ google-drive-ocamlfuse -xdgbd -label monitor -serviceaccountpath "${1}/monitor-r
 
 python ${1}/sus-kpis-analysis/sia/etls/lib/gdfuse_config.py -c "${1}/gdfuse/monitor/config" -t "${3}"
 
-!google-drive-ocamlfuse -cc -label monitor ${1}/datalake
+google-drive-ocamlfuse -cc -label monitor ${1}/datalake
