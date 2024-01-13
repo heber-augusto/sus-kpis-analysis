c18204106fbb98345bfdadd9d1427b5eb6550a43
Heber Scachetti <heber.augusto@gmail.com>
2024-01-13 16:27:31 -0300

    Create mount_google_drive.sh

---
+++ sia/etls/bin/mount_google_drive.sh
@@ -0,0 +1,12 @@
+add-apt-repository --yes ppa:alessandro-strada/ppa
+apt-get update
+apt-get install google-drive-ocamlfuse
+
+mkdir "${1}/datalake"
+mkdir "${1}/gdfuse"
+
+google-drive-ocamlfuse -xdgbd -label monitor -serviceaccountpath "${1}/monitor-rosa-leitura.json" -serviceaccountuser "${2}"
+
+python ${1}/sus-kpis-analysis/sia/etls/lib/gdfuse_config.py -c "${1}/gdfuse/monitor/config" -t "${3}"
+
+!google-drive-ocamlfuse -cc -label monitor ${1}/datalake
