122b8df2bd786d1591ce90dc263dc529c528a96b
Heber Scachetti <heber.augusto@gmail.com>
2024-03-13 12:22:55 -0300

    Create mount_google_drive_v2.sh

---
+++ sia/etls/bin/mount_google_drive_v2.sh
@@ -0,0 +1,10 @@
+mkdir "${1}/${4}"
+mkdir "${1}/gdfuse"
+
+fusermount -u "${1}/${4}"
+
+google-drive-ocamlfuse -xdgbd -label "${4}" -serviceaccountpath "${5}" -serviceaccountuser "${2}"
+
+python ${6}/sus-kpis-analysis/sia/etls/lib/gdfuse_config.py -c "${1}/gdfuse/${4}/config" -t "${3}"
+
+google-drive-ocamlfuse -cc -label ${4} ${1}/${4}
